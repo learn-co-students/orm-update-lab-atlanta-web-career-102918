df9a9924c05a0d9bab4697ef9b6622422b0b36c5
Andrew Jones <joneseyit@protonmail.com>
2018-11-10 19:42:44 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -41,11 +41,19 @@ class Student
 
 
   def self.find_by_name(name)
-    sql = "SELECT * FROM students WHERE name = ?"
-    result = DB[:conn].execute(sql, name)[0]
-    Student.new(result[0], result[1], result[2])
-  end
+     # find the student in the database given a name
+     # return a new instance of the Student class
+     sql = <<-SQL
+       SELECT *
+       FROM students
+       WHERE name = ?
+       LIMIT 1
+     SQL
 
+     DB[:conn].execute(sql,name).map do |row|
+       self.new_from_db(row)
+     end.first
+   end
   def save
     if self.id
       self.update
